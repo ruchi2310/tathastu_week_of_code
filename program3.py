d25e84f5e679121b7ce3811725a7c2d20982b0cf
Ruchika jaiswal <65599690+ruchi2310@users.noreply.github.com>
2020-05-20 20:45:01 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,9 @@
+# tathastu_week_of_code
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+a=a+b
+b=a-b
+a=a-b
+print("after swapping")
+print("a of value=",a)
+print("b of value=",b)
