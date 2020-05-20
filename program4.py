70d23b931b2796d0ff6f9a219d55e93669eac759
Ruchika jaiswal <65599690+ruchi2310@users.noreply.github.com>
2020-05-20 20:51:09 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,7 @@
+# tathastu_week_of_code
+cp=float(input("enter the cp of the product"))
+sp=float(input("enter the sp of the product"))
+profit= sp-cp
+newsellingprice=1.05*profit+cp
+print("progit from sell is",profit)
+print("increase profit by 5% the selling price should be",newsellingprice)
