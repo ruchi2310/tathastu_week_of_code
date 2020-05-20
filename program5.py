7a84ac6afc6bc562d9a6b9a687d440bf8d94d975
Ruchika jaiswal <65599690+ruchi2310@users.noreply.github.com>
2020-05-20 21:03:17 +0530

    Create program5.py

---
+++ program5.py
@@ -0,0 +1,16 @@
+# tathastu_week_of_code
+runplayer1=int(input("enter the run scored by player 1 in 60 balls"))
+runplayer2=int(input("enter the run scored by player  2 in 60 balls"))
+runplayer3=int(input("enter the run scored by player 3 in 60 balls"))
+strikerate1=runplayer1*100/60
+strikerate2=runplayer2*100/60
+strikerate3=runplayer3*100/60
+print("strike rate of player 1 is",strikerate1)
+print("strike rate of player 2 is",strikerate2)
+print("strike rate of player 3 is",strikerate3)
+print("run scored by player 1 if they played 60 more ball is",runplayer1*2)
+print("run scored by player 2 if they played 60 more ball is",runplayer2*2)
+print("run scored by player 2 if they played 60 more ball is",runplayer3*2)
+print("maximum no.of sixes player 1 could hit=",runplayer1//6)
+print("maximum no.of sixes player 2 could hit=",runplayer1//6)
+print("maximum no.of sixes player 3 could hit=",runplayer1//6)
