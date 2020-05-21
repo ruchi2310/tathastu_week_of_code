1c2fee4da20a2ace7648570f7e764624314d9fd1
Ruchika jaiswal <65599690+ruchi2310@users.noreply.github.com>
2020-05-21 20:58:42 +0530

    Create program 4.py

---
+++ day 2/program 4.py
@@ -0,0 +1,6 @@
+# tathastu_week_of_code
+n=int(input("enter the value of n"))
+for i in range(n):
+    print("* "*(n-i)+"  "*i+ " *"*(n-i))
+for i in range(2,n+1):
+    print("* "*i +" "*(n-i)+ " *" *i)  
