d51212039b8e0193d1bb82b71c6853cfcc6810bd
Ruchika jaiswal <65599690+ruchi2310@users.noreply.github.com>
2020-05-25 21:13:13 +0530

    Create program 1.py

---
+++ day 5/program 1.py
@@ -0,0 +1,6 @@
+# tathastu_week_of_code
+def replace_0_with_5(number):
+    return int(str(number).replace('0','5'))
+number=int(input("enter the number you want to check"))
+print("the number after replacing all 0 with 5 will be",replace_0_with_5(number))
+    
