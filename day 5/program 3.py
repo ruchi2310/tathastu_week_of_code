389cf269d83fa40e725037d767505f6d8e7a8e55
Ruchika jaiswal <65599690+ruchi2310@users.noreply.github.com>
2020-05-25 21:15:21 +0530

    Create program 3.py

---
+++ day 5/program 3.py
@@ -0,0 +1,9 @@
+# tathastu_week_of_code
+def replaceGreaterInteger(List):
+    for i in range(size-1)
+    List[i]=max(List[i+1:])
+    return Listsize=int(input("enter the size of the list"))
+    List=[]
+    for i in range(size):
+        List.append(int(input("enter the element number"+ str)i+1) +" in the list")
+print("the list after replacing every element with greatest element on right side is ",replaceGreaterInteger(List))
